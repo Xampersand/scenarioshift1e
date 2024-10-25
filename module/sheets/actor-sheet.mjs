e561410446ffad40e3973dae63dd2261129db8d7
Vankell <badeageorgeandrei@gmail.com>
2024-10-25 21:59:20 +0300

    merged weapon .less file created ranged weapon sheet fixed 1 minor typo in actor sheet roll function added transparency color in config

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -230,7 +230,7 @@ export class SS1EActorSheet extends ActorSheet {
 		// Handle rolls that supply the formula directly.
 		if (dataset.roll) {
 			let label = dataset.label ? `[attribute] ${dataset.label}` : '';
-			let roll = new Roll(dataset.roll, this.actor.getRollData());
+			let roll = new roll(dataset.roll, this.actor.getRollData());
 			roll.toMessage({
 				speaker: ChatMessage.getSpeaker({ actor: this.actor }),
 				flavor: label,
