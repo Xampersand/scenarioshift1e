825ffe7f68c29c0ea9e468446a3c7906c92836c6
xand <thatzenguydev@gmail.com>
2024-10-29 16:09:58 +0200

    inv part 5.1

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -4,12 +4,37 @@
  * @return {Promise}
  */
 export const preloadHandlebarsTemplates = async function () {
-  await loadTemplates([
-    // Actor partials.
-	'systems/ss1e/templates/actor/parts/inventory.hbs',
-    // Item partials
-    'systems/ss1e/templates/item/parts/item-effects.hbs',
-  ]);
-  const templatePath = await getTemplate('systems/ss1e/templates/actor/parts/inventory.hbs');
-  Handlebars.registerPartial('inventory', templatePath);
+	Handlebars.registerHelper("repeat", function (times, opts) {
+		var out = "";
+		var i;
+		var data = {};
+
+		if (times) {
+			for (i = 0; i < times; i += 1) {
+				data.index = i;
+				out += opts.fn(this, {
+					data: data
+				});
+			}
+		} else {
+
+			out = opts.inverse(this);
+		}
+
+		return out;
+	});
+
+	await loadTemplates([
+		// Actor partials.
+		'systems/ss1e/templates/actor/parts/inventory.hbs',
+		// Item partials
+		'systems/ss1e/templates/item/parts/item-effects.hbs',
+	]);
+	const templatePath = await getTemplate('systems/ss1e/templates/actor/parts/inventory.hbs');
+	Handlebars.registerPartial('inventory', templatePath);
+
+	/*
+  * Repeat given markup with given times
+  * provides @index for the repeated iteraction
+  */
 };
