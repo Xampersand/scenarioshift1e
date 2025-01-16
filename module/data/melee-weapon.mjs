cee62b73a145f97bad50eae5091adaf6f4f8a62e
Vankell <badeageorgeandrei@gmail.com>
2025-01-16 19:31:18 +0200

    can have 0 dice

--- module/data/melee-weapon.mjs
+++ module/data/melee-weapon.mjs
@@ -3,14 +3,18 @@ import SS1EWeapon from './weapon.mjs';
 export default class SS1EMeleeWeapon extends SS1EWeapon {
 	static defineSchema() {
 		const fields = foundry.data.fields;
-		const requiredInteger = { required: true, nullable: false, integer: true };
+		const requiredInteger = {
+			required: true,
+			nullable: false,
+			integer: true,
+		};
 		const schema = super.defineSchema();
 
 		schema.damageRoll = new fields.SchemaField({
 			diceNum: new fields.NumberField({
 				...requiredInteger,
 				initial: 1,
-				min: 1,
+				min: 0,
 			}),
 			diceSize: new fields.StringField({ initial: 'd4' }),
 			diceBonus: new fields.NumberField({
